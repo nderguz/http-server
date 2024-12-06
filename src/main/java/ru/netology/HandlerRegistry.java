2292d02b61b56b232dc1eef557b6dc386445ca92
nikolai <nderguz@yandex.ru>
2024-12-06 20:05:03 +0300

    refactor HandlerRegistry

--- src/main/java/ru/netology/HandlerRegistry.java
+++ src/main/java/ru/netology/HandlerRegistry.java
@@ -2,7 +2,6 @@ package ru.netology;
 
 import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
@@ -11,38 +10,31 @@ import java.util.List;
 public class HandlerRegistry {
 
     private final Server server;
-    private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    private final List<String> validPaths;
 
-    public HandlerRegistry(Server server) {
+    public HandlerRegistry(Server server, List<String> validPaths) {
         this.server = server;
+        this.validPaths = validPaths;
     }
 
     public void registerHandlers() {
-        registerFiles();
-    }
-
-    private void registerFiles() {
-        for(var path: validPaths) {
+        for (var path : validPaths) {
             server.addHandler("GET", path, (request, responseStream) -> {
                 try {
                     Path filePath = Path.of(".", "public", path);
                     var mimeType = Files.probeContentType(filePath);
                     sendResponse(responseStream, filePath, mimeType, path);
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
         }
-
     }
 
     private void sendResponse(BufferedOutputStream out, Path filePath, String mimeType, String path) throws IOException {
         if (path.equals("/classic.html")) {
             final var template = Files.readString(filePath);
-            final var content = template.replace(
-                    "{time}",
-                    LocalDateTime.now().toString()
-            ).getBytes();
+            final var content = template.replace("{time}", LocalDateTime.now().toString()).getBytes();
             prepareResponse(out, mimeType, content.length, content);
         } else {
             final var length = Files.size(filePath);
@@ -50,25 +42,8 @@ public class HandlerRegistry {
         }
     }
 
-    private void sendInvalidPathError(OutputStream out) throws IOException {
-        out.write((
-                "HTTP/1.1 404 Not Found\r\n" +
-                        "Content-Length: 0\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n"
-        ).getBytes());
-        out.flush();
-    }
-
     private String prepareHeaders(String contentType, long contentLength) {
-        return String.format(
-                "HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: %s\r\n" +
-                        "Content-Length: %d\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n",
-                contentType, contentLength
-        );
+        return String.format("HTTP/1.1 200 OK\r\n" + "Content-Type: %s\r\n" + "Content-Length: %d\r\n" + "Connection: close\r\n" + "\r\n", contentType, contentLength);
     }
 
     private void prepareResponse(BufferedOutputStream out, String contentType, long contentLength, Path filePath) throws IOException {
--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,10 +1,26 @@
 package ru.netology;
 
+import java.util.List;
+
 public class Main {
+
+    private static final List<String> PATH_LIST = List.of(
+            "/index.html",
+            "/spring.svg",
+            "/spring.png",
+            "/resources.html",
+            "/styles.css",
+            "/app.js",
+            "/links.html",
+            "/forms.html",
+            "/classic.html",
+            "/events.html",
+            "/events.js");
+
     public static void main(String[] args) {
 
         final var server = new Server(64);
-        final var registry = new HandlerRegistry(server);
+        final var registry = new HandlerRegistry(server, PATH_LIST);
 
         registry.registerHandlers();
         server.listen(9999);
